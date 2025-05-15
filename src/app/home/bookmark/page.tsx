94119b1116009612e968c0a408cb1a4f708ca3d1
vanijain651 <vani.jain@aristhrottle.org>
2025-05-16 00:01:44 +0530

    Added bookmarks UI for mobile UI and also fixed UI in desktop

--- src/app/home/bookmark/page.tsx
+++ src/app/home/bookmark/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-
 import { Context } from "@/context/contextProvider";
-
 import React, { useContext, useEffect, useState } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 type Props = {};
-
-// interface TabButtonProps {
-//   label: string;
-//   isActive: boolean;
-// }
 
 interface MyVotedMeme {
   [key: string]: { id: string; name: string; image_url: string };
 }
 
 export default function Page({}: Props) {
-
   const [loading, setLoading] = useState<boolean>(false);
   const [memes, setMemes] = useState<MyVotedMeme>();
 
   const { userDetails } = useContext(Context);
-
-
 
   useEffect(() => {
     getMyMemes();
@@ -48,64 +38,36 @@ export default function Page({}: Props) {
   };
 
   return (
-    <div className="flex flex-col max-w-7xl mx-auto p-8">
-      <h2 className="cursor-pointer text-[#29e0ca] text-4xl font-medium text-center mb-10">
+    <div className="flex flex-col items-center justify-center mx-4 md:mx-0 md:ml-20 lg:ml-0">
+      <h2 className="text-[#29e0ca] text-2xl md:text-4xl font-medium text-center mb-5 md:mb-10">
         My Bookmarks
       </h2>
 
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full md:max-w-4xl mb-4">
         {memes &&
           Object.entries(memes).map((item, index) => (
-            <div key={index} className="p-4">
-              <div className="flex gap-4">
-                <div className="relative flex-grow">
-                  <img
-                    src={item[1].image_url}
-                    alt="Content"
-                    className="w-full aspect-square object-cover border-2 border-white"
-                  />
-                  <div className="flex justify-between">
-                    <p>{item[1].name}</p>
-                  </div>
-                </div>
-
-                {/* <div className="flex flex-col justify-between mb-7">
-                <div>
-                  <p className="text-[#1783fb] text-lg font-bold">
-                    {item.votes}
+            <div key={index} className="flex justify-center">
+              <div className="w-full max-w-sm rounded overflow-hidden shadow-md">
+                <img
+                  src={item[1].image_url}
+                  alt="Content"
+                  className="w-full aspect-square object-cover border-2 border-white"
+                />
+                <div className="flex justify-between">
+                  <p className="font-medium text-lg md:text-xl">
+                    {item[1].name}
                   </p>
                 </div>
-
-                <div className="flex flex-col gap-4">
-                  <div className="flex flex-col items-center">
-                    <FaRegEye className="rotate-90" size={22} />
-                    <span className="text-base text-[#1783fb]">
-                      {item.views}
-                    </span>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <FaRegShareFromSquare size={22} />
-                    <span className="text-base text-[#1783fb]">
-                      {item.shares}
-                    </span>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <FaRegBookmark size={22} />
-                    <span className="text-base text-[#1783fb]">
-                      {item.saves}
-                    </span>
-                  </div>
-                </div>
-              </div> */}
               </div>
             </div>
           ))}
-        <div className="col-span-3">
+
+        <div className="col-span-full">
           {loading && (
-            <AiOutlineLoading3Quarters className="animate-spin text-3xl mx-auto col-span-12" />
+            <AiOutlineLoading3Quarters className="animate-spin text-3xl mx-auto" />
           )}
           {!loading && !memes && (
-            <p className="text-center text-nowrap text-2xl mx-auto col-span-12">
+            <p className="text-center text-white text-2xl mx-auto">
               Meme not found
             </p>
           )}
