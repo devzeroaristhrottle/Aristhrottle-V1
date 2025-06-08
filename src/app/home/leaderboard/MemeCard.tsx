9ade8e61296d97cc3f85e950eab5fd52b6401995
vanijain651 <vani.jain@aristhrottle.org>
2025-06-08 17:02:57 +0530

    Refactor LeaderboardMemeCard layout and update upvote/downvote icons

--- src/app/home/leaderboard/MemeCard.tsx
+++ src/app/home/leaderboard/MemeCard.tsx
@@ -7,6 +7,7 @@ import Share from "@/components/Share";
 import { useMemeActions } from "../bookmark/bookmarkHelper";
 import { CiBookmark } from "react-icons/ci";
 import { useUser } from "@account-kit/react";
+import { BiDownArrow, BiUpArrow } from "react-icons/bi";
 
 export const LeaderboardMemeCard: React.FC<{
   meme: LeaderboardMeme;
@@ -40,20 +41,20 @@ export const LeaderboardMemeCard: React.FC<{
 
   return (
     <div className="p-4 md:p-4 w-full lg:mx-auto">
-      <div className="flex justify-between items-center md:mb-1 md:mr-20">
-        <div className="flex items-center gap-x-1 md:gap-x-2">
-          <CgProfile className="md:w-7 md:h-7" />
-          <span className="text-[#29e0ca] text-base md:text-2xl">
-            {meme.created_by.username}
-          </span>
-        </div>
-        <p className="text-[#29e0ca] text-base md:text-2xl font-medium">
-          #{meme.rank}
-        </p>
-      </div>
       <div className="flex flex-col md:flex-row gap-x-1">
-        <div className="">
-          <div className="w-full h-full sm:w-[16.875rem] sm:h-[16.875rem] md:w-[16rem] md:h-[16.875rem] lg:w-[15.625rem] lg:h-[15.625rem] xl:w-[22rem] xl:h-[22rem] object-cover border-2 border-white">
+        <div className="flex flex-col">
+          <div className="username_rank_wrapper flex justify-between items-center md:mb-1">
+            <div className="flex items-center gap-x-1">
+              <CgProfile className="md:w-6 md:h-6" />
+              <span className="text-[#29e0ca] text-base md:text-2xl">
+                {meme.created_by.username}
+              </span>
+            </div>
+            <p className="text-[#29e0ca] text-base md:text-2xl font-medium">
+              #{meme.rank}
+            </p>
+          </div>
+          <div className="image_wrapper w-full h-full sm:w-[16.875rem] sm:h-[16.875rem] md:w-[16rem] md:h-[16.875rem] lg:w-[15.625rem] lg:h-[15.625rem] xl:w-[22rem] xl:h-[22rem] object-cover border-2 border-white">
             <img
               onClick={() => {
                 onOpenMeme();
@@ -63,26 +64,21 @@ export const LeaderboardMemeCard: React.FC<{
               className="w-full h-full"
             />
           </div>
-          <div className="flex justify-between text-lg leading-tight md:text-xl">
+          <div className="title_wrapper flex justify-between text-lg leading-tight md:text-xl">
             <p>{meme.name}</p>
             {/* <p>{meme.createdAt.split("T")[0]}</p> */}
           </div>
         </div>
-        <div className="flex flex-row md:flex-col justify-between">
-          <p className="text-[#1783fb] text-lg md:text-xl font-bold ml-2">
+        <div className="flex flex-row md:flex-col justify-between ml-1 md:pt-8 md:pb-4">
+          <p className="text-[#1783fb] text-lg md:text-xl font-bold">
             {meme.in_percentile.toFixed(2)}%
           </p>
-          <div className="flex flex-row justify-center md:justify-normal md:flex-col items-start gap-y-5 gap-x-6 md:gap-x-0 mb-14 md:mb-4">
-            <div className="flex flex-col items-center">
+          <div className="flex flex-row justify-center md:justify-normal md:flex-col items-center md:items-start gap-y-0 md:gap-y-5 gap-x-4 md:gap-x-0">
+            <div className="flex flex-row md:flex-col items-start gap-x-0.5 md:gap-y-0 lg:gap-y-2">
               {activeTab === "all" && (
-                <div
-                  title="upvote"
-                  className="upvote-img-wrapper cursor-pointer rotate-180"
-                >
-                  <img
-                    src={"/assets/upvote.svg"}
-                    alt="vote"
-                    className="w-4 h-4 md:w-10 md:h-10"
+                <div title="upvote" className="upvote-wrapper cursor-pointer">
+                  <BiUpArrow
+                    className="w-3 h-3 md:w-5 md:h-5 lg:w-7 lg:h-7"
                     onClick={() => {
                       if (onUpvoteDownvote) {
                         onUpvoteDownvote(meme._id, "upvote");
@@ -92,36 +88,36 @@ export const LeaderboardMemeCard: React.FC<{
                 </div>
               )}
 
-              <img
-                src={"/assets/vote-logo.svg"}
-                alt="vote"
-                className="w-4 h-4 md:w-7 md:h-7 cursor-not-allowed"
-              />
-              <span className="text-base md:text-2xl text-[#1783fb]">
-                {meme.vote_count}
-              </span>
+              <div className="flex flex-col md:flex-row items-center justify-center gap-x-2">
+                <img
+                  src={"/assets/vote-logo.svg"}
+                  alt="vote"
+                  className="w-4 h-4 md:w-5 md:h-5 lg:w-7 lg:h-7 cursor-not-allowed"
+                />
+                <span className="text-base md:text-2xl text-[#1783fb]">
+                  {meme.vote_count}
+                </span>
+              </div>
 
               {activeTab === "all" && (
                 <div
                   title="downvote"
-                  className="downvote-img-wrapper cursor-pointer -mt-3"
+                  className="downvote-wrapper cursor-pointer"
                 >
-                  <img
-                    src={"/assets/downvote.svg"}
-                    alt="vote"
-                    className="w-4 h-4 md:w-10 md:h-10"
+                  <BiDownArrow
+                    className="w-3 h-3 md:w-5 md:h-5 lg:w-7 lg:h-7"
                     onClick={() => {
                       if (onUpvoteDownvote) {
-                        onUpvoteDownvote(meme._id, "upvote");
+                        onUpvoteDownvote(meme._id, "downvote");
                       }
                     }}
                   />
                 </div>
               )}
             </div>
-            <div className="flex flex-col items-center ml-2">
+            <div className="flex flex-col items-center">
               <FaRegShareFromSquare
-                className="w-4 h-4 md:w-7 md:h-7 cursor-pointer"
+                className="w-4 h-4 md:w-5 md:h-5 lg:w-7 lg:h-7 cursor-pointer"
                 onClick={() => {
                   setIsShareOpen(true);
                 }}
@@ -129,11 +125,11 @@ export const LeaderboardMemeCard: React.FC<{
               <span className="text-lg md:text-2xl text-[#1783fb]">0</span>
             </div>
             {user && user.address ? (
-              <div>
+              <div className="-ml-1">
                 {isBookmarked ? (
-                  <div className="flex flex-col items-center cursor-pointer ml-1">
+                  <div className="flex flex-col items-center cursor-pointer">
                     <FaBookmark
-                      className="w-4 h-4 md:w-7 md:h-7"
+                      className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                       onClick={() => {
                         handleBookmark(meme._id, meme.name, meme.image_url);
                         getBookmarks();
@@ -144,9 +140,9 @@ export const LeaderboardMemeCard: React.FC<{
                     </span>
                   </div>
                 ) : (
-                  <div className="flex flex-col items-center cursor-pointer ml-1">
+                  <div className="flex flex-col items-center cursor-pointer">
                     <CiBookmark
-                      className="w-4 h-4 md:w-7 md:h-7"
+                      className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                       onClick={() => {
                         handleBookmark(meme._id, meme.name, meme.image_url);
                         getBookmarks();
