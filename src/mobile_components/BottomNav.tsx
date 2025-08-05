d8a57d27a9e262fb656b3ca64ec62523d3fe4d09
bikram-aristhrottle <bikram.ghuku@aristhrottle.org>
2025-08-06 01:25:05 +0530

    feat: update bottom navbar

--- src/mobile_components/BottomNav.tsx
+++ src/mobile_components/BottomNav.tsx
@@ -1,34 +1,51 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
-import { FaRegBookmark } from 'react-icons/fa'
-import { GrCloudUpload } from 'react-icons/gr'
-import { IoPodiumOutline } from 'react-icons/io5'
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa'
+import { IoCloudUpload, IoCloudUploadOutline } from 'react-icons/io5'
+import { IoPodium, IoPodiumOutline } from 'react-icons/io5'
 import { LuTrophy } from 'react-icons/lu'
 
 function BottomNav() {
+	const pathname = usePathname();
+
 	return (
 	<div className="bottom-0 w-full bg-black/90 py-3 flex flex-row justify-evenly items-center">
-		<div>
+		<Link href="/mobile/rewards">
 			<LuTrophy className="h-6 w-6" />
-		</div>
-		<div>
-			<GrCloudUpload className="h-6 w-6" />
-		</div>
-		<div>
-			<Image
-				alt="side-bar-logo"
-				height={48}
-				quality={100}
-				src="/assets/aris-logo.svg"
-				width={48}
-			/>		
-		</div>
-		<div>
-			<IoPodiumOutline className="h-6 w-6" />
-		</div>
+		</Link>
+		<Link href="/mobile/upload">
+			{pathname === '/mobile/upload' ? (
+				<IoCloudUpload className="h-6 w-6" />
+			) : (
+				<IoCloudUploadOutline className="h-6 w-6" />
+			)}
+		</Link>
+		<Link href="/mobile">
+			<div className={pathname === '/mobile' ? 'scale-110' : ''}>
+				<Image
+					alt="side-bar-logo"
+					height={48}
+					quality={100}
+					src="/assets/aris-logo.svg"
+					width={48}
+				/>
+			</div>
+		</Link>
+		<Link href="/mobile/leaderboard">
+			{pathname === '/mobile/leaderboard' ? (
+				<IoPodium className="h-6 w-6" />
+			) : (
+				<IoPodiumOutline className="h-6 w-6" />
+			)}
+		</Link>
 		<Link href="/mobile/saved">
-			<FaRegBookmark className="h-6 w-6" />
+			{pathname === '/mobile/saved' ? (
+				<FaBookmark className="h-6 w-6" />
+			) : (
+				<FaRegBookmark className="h-6 w-6" />
+			)}
 		</Link>
 	</div>)
 }
